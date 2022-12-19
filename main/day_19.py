8dc0922bd8a203a7f7d300a2db53a0b2ef972d0a
houston-d <dougalhouston@gmail.com>
2022-12-19 20:42:51 +0000

    part 1 working again

--- main/day_19.py
+++ main/day_19.py
@@ -26,8 +26,7 @@ def add_resource(state):
     return state
 
 
-def calc_max(blueprint, time):
-
+def calc_max(blueprint, t):
     max_ = 0
     # 0: ore
     # 1: ore robot count
@@ -40,15 +39,17 @@ def calc_max(blueprint, time):
     # 8: time left
 
     queue = deque()
-    queue.append([0, 1, 0, 0, 0, 0, 0, 0, time])
+    queue.append([0, 1, 0, 0, 0, 0, 0, 0, t])
 
     seen = set()
-
+    count = 55
     # We don't need to produce more robots than the max required for a new robot
     # as only one robot can be made at one time
     max_ore_prod = max([blueprint[0], blueprint[1], blueprint[2][0], blueprint[3][0]])
     max_clay_prod = blueprint[2][1]
     max_obs_prod = blueprint[3][1]
+
+    max_geode_robot = 0
 
     while len(queue) > 0:
         current = queue.popleft()
@@ -59,22 +60,37 @@ def calc_max(blueprint, time):
             seen.add(check)
         if current[6] > max_:
             max_ = current[6]
+            print(max_)
+
         if current[8] == 0:
             continue
+
+        # if len(seen) % 1000000 == 0:
+        #     print(max_, len(seen))
+        if max_ > current[7] * current[8] + current[6] and t < 5:
+            continue
+        # if current[7] > max_geode_robot:
+        #     max_geode_robot = current[7]
+        # elif current[7] < (max_geode_robot) // 2:
+        #     continue
+
+        # if current[8] < count:
+        #     print(current[8], len(queue))
+        #     count = current[8]
         # create new ore robot
         o = blueprint[0]
         if current[0] >= o and current[1] < max_ore_prod:
             new = add_resource(current.copy())
             new[0] -= o
             new[1] += 1
-            queue.append(new)
+            queue.appendleft(new)
         # create new clay robot
         o = blueprint[1]
         if current[0] >= o and current[3] < max_clay_prod:
             new = add_resource(current.copy())
             new[0] -= o
             new[3] += 1
-            queue.append(new)
+            queue.appendleft(new)
         # create new obsidian robot
         o, c = blueprint[2]
         if current[0] >= o and current[2] >= c and current[5] < max_obs_prod:
@@ -82,7 +98,7 @@ def calc_max(blueprint, time):
             new[0] -= o
             new[2] -= c
             new[5] += 1
-            queue.append(new)
+            queue.appendleft(new)
         # create new geode robot
         o, o_ = blueprint[3]
         if current[0] >= o and current[4] >= o_:
@@ -90,7 +106,7 @@ def calc_max(blueprint, time):
             new[0] -= o
             new[4] -= o_
             new[7] += 1
-            queue.append(new)
+            queue.appendleft(new)
 
         new = add_resource(current.copy())
         queue.append(new)
@@ -142,7 +158,7 @@ if __name__ == "__main__":
     file = "../resources/19_1.txt"
     start = time.time()
 
-    # process_lines_1(readfile(file))
-    process_lines_2(readfile(file))
+    process_lines_1(readfile(file))
+    # process_lines_2(readfile(file))
     end = time.time()
     print(end - start)
